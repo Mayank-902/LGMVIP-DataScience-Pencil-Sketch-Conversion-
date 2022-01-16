e7c836eca57d94f33820a3eb27ae60a7cfb6f6fb
MAYANK RAJ <mayank.09raj@gmail.com>
2022-01-16 18:57:47 +0530

    final commit

--- img_2_pencil.py
+++ img_2_pencil.py
@@ -1,20 +1,18 @@
 import numpy as np
 import cv2 as cv
 
-# defining Function Method
-
 def convert(image):
     gray=cv.cvtColor(image,cv.COLOR_BGR2GRAY)     #coversion to gray scale image
-    inv_gray=255-gray                             #inverting the gray image
+    inv_gray=255-gray                             #inverting the gray image by subtracting from 255
     blur=cv.GaussianBlur(inv_gray,(21,21),0)      #blur the inverted gray image
-    inv_blur=255-blur                             #inverting the blurred image
-    pencil=cv.divide(gray,inv_blur,scale=256.0)   # diving the gray and inverted blurr image for output pencil sketch
+    inv_blur=255-blur                             #inverting the blurred image 
+    pencil=cv.divide(gray,inv_blur,scale=256.0)   #diving the gray and inverted blurr image for output pencil sketch
     return pencil
 
-img=str(input("Enter the Image location: "))      # D:\manor\Pictures\Saved Pictures\naruto.jpg
+img=str(input("Enter the Image location: "))      # D:\manor\Pictures\Saved Pictures\dog.jpg
 
-img=cv.imread(img)
-image=np.copy(img)
+img=cv.imread(img)                                #Read the image from location
+image=np.copy(img)                                #copy the object image for defined function using numpy.copy
 ps_image=convert(image)
 
 cv.imshow('Original Image',image)
--- imgtosktch.py
+++ imgtosktch.py
@@ -1,12 +1,10 @@
-#import the library , if you don't have, command to install it is : pip install opencv-python
+#import the opencv library , if you don't have, command to install it is : pip install opencv-python
 import cv2
 
-#Direct Method 
-
 img=str(input("Give Image Location: "))                # D:\manor\Pictures\Saved Pictures\naruto.jpg
-img= cv2.imread(img)
-gray_img =cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)        #coversion to gray scale image
-inv_gray_img = 255 - gray_img                          #inverting the gray image
+img= cv2.imread(img)                                   #Read the image from location 
+gray_img =cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)        #coversion to gray scale image 
+inv_gray_img = 255 - gray_img                          #inverting the gray image by subtracting from 255
 blurr_img = cv2.GaussianBlur(inv_gray_img, (21,21) ,0) #blur the inverted gray image
 inv_blurr_img = 255 - blurr_img                        #inverting the blurred image
 
@@ -19,4 +17,5 @@ cv2.imshow('Original image',img)
 # cv2.imshow('Blurred image',blurr_img)
 # cv2.imshow('Inverted blurred image',inv_blurr_img)
 cv2.imshow('Pencil sketch image',pencilsketch_img)
-cv2.waitKey(0)
+cv2.waitKey(0)         #{ waitkey() allows to display for given milisecond, 
+                       #  and 0 argument allows to hold window till any key press  }
